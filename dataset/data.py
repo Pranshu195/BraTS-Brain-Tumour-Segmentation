8e4f946c1421e0bda7c370f659782de530e857f7
pranshu7 <pranshu7@gitlab.com>
2021-02-09 12:33:11 +0530

    survival info analizing

--- dataset/data.py
+++ dataset/data.py
@@ -2,11 +2,13 @@ import torch
 import cv2
 import os
 import nibabel as nib
+import pandas as pd
 import numpy as np
+from sklearn.model_selection import StratifiedKFold
 import matplotlib.pyplot as plt
 
 #Analyse
-data_file1 = os.path.join("./MICCAI_BraTS2020_TrainingData/BraTS20_Training_001/BraTS20_Training_001_flair.nii.gz")
+data_file1 = os.path.join("./dataset/MICCAI_BraTS2020_TrainingData/BraTS20_Training_001/BraTS20_Training_001_flair.nii.gz")
 data = nib.load(data_file1)
 print(data.shape)
 nd_arr = np.asarray(data)
@@ -14,9 +16,9 @@ print(nd_arr.shape)
 
 
 class GlobalConfig:
-    root_dir = '../dataset'
-    train_root_dir = '../dataset/MICCAI_BraTS2020_TrainingData'
-    test_root_dir = '../dataset/MICCAI_BraTS2020_ValidationData'
+    root_dir = './dataset'
+    train_root_dir = './dataset/MICCAI_BraTS2020_TrainingData'
+    test_root_dir = './dataset/MICCAI_BraTS2020_ValidationData'
     path_to_csv = './train_data.csv'
     pretrained_model_path = ''
     seed = 3333
@@ -31,7 +33,49 @@ def seed_everything(seed: int):
 config = GlobalConfig()
 seed_everything(config.seed)
 
-survival_info_df = pd.read_csv('../dataset/MICCAI_BraTS2020_TrainingData/survival_info.csv')
-name_mapping_df = pd.read_csv('../dataset/MICCAI_BraTS2020_TrainingData/name_mapping.csv')
+survival_info_df = pd.read_csv('./dataset/MICCAI_BraTS2020_TrainingData/survival_info.csv')
+name_mapping_df = pd.read_csv('./dataset/MICCAI_BraTS2020_TrainingData/name_mapping.csv')
 
 
+
+name_mapping_df.rename({'BraTS_2020_subject_ID': 'Brats20ID'}, axis=1, inplace=True) 
+
+
+df = survival_info_df.merge(name_mapping_df, on="Brats20ID", how="right")
+
+paths = []
+for _, row  in df.iterrows():
+    
+    id_ = row['Brats20ID']
+    phase = id_.split("_")[-2]
+    
+    if phase == 'Training':
+        path = os.path.join(config.train_root_dir, id_)
+    else:
+        path = os.path.join(config.test_root_dir, id_)
+    paths.append(path)
+    
+df['path'] = paths
+
+#split data on train, test, split
+#train_df, val_df = train_test_split(train_df, test_size=0.1, random_state=69, shuffle=True)
+#train_df, val_df = train_df.reset_index(drop=True), val_df.reset_index(drop=True)
+
+train_data = df.loc[df['Age'].notnull()].reset_index(drop=True)
+train_data["Age_rank"] =  train_data["Age"] // 10 * 10
+train_data = train_data.loc[train_data['Brats20ID'] != 'BraTS20_Training_355'].reset_index(drop=True, )
+
+skf = StratifiedKFold(
+    n_splits=7, random_state=config.seed, shuffle=True
+)
+for i, (train_index, val_index) in enumerate(skf.split(train_data, train_data["Age_rank"])):
+        train_data.loc[val_index, "fold"] = i
+
+train_df = train_data.loc[train_data['fold'] != 0].reset_index(drop=True)
+val_df = train_data.loc[train_data['fold'] == 0].reset_index(drop=True)
+
+test_df = df.loc[~df['Age'].notnull()].reset_index(drop=True)
+print("train_df ->", train_df.shape, "val_df ->", val_df.shape, "test_df ->", test_df.shape)
+train_data.to_csv("train_data.csv", index=False)
+
+print(train_df.head)
